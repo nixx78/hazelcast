53a62fde8ac6c71b0071662292cce0d1f3c20c5f
nixx78 <nixx78@gmail.com>
2023-08-15 17:32:11 +0300

    Minor test

--- hazelcast-poc/src/test/java/lv/nixx/poc/hazelcast/CollectionsSandbox.java
+++ hazelcast-poc/src/test/java/lv/nixx/poc/hazelcast/CollectionsSandbox.java
@@ -17,92 +17,101 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class CollectionsSandbox {
 
-	private HazelcastInstance hz;
-	private final DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+    private HazelcastInstance hz;
+    private final DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
 
-	private final String personsForCompanyIdMap = "personsByCompanyId";
-	private IMap<Integer, Person> personMap;
+    private final String personsForCompanyIdMap = "personsByCompanyId";
+    private IMap<Integer, Person> personMap;
 
-	@Before
-	public void init() {
-		Config config = new Config();
+    @Before
+    public void init() {
+        Config config = new Config();
 
-		MultiMapConfig mmConfig = new MultiMapConfig();
-		mmConfig.setName(personsForCompanyIdMap);
-		mmConfig.setBinary(false); // Decrease performance, but compare objects using equals/hashCode
-		mmConfig.setValueCollectionType("SET"); // No duplicates for one company
+        MultiMapConfig mmConfig = new MultiMapConfig();
+        mmConfig.setName(personsForCompanyIdMap);
+        mmConfig.setBinary(false); // Decrease performance, but compare objects using equals/hashCode
+        mmConfig.setValueCollectionType("SET"); // No duplicates for one company
 
-		config.addMultiMapConfig(mmConfig);
+        config.addMultiMapConfig(mmConfig);
 
-		hz = HazelcastTestInstance.get(config);
+        hz = HazelcastTestInstance.get(config);
 
-		personMap = hz.getMap("persons.map");
-		personMap.clear();
-	}
+        personMap = hz.getMap("persons.map");
+        personMap.clear();
+    }
 
-	@Test
-	public void multiMapSample() throws ParseException {
+    @Test
+    public void multiMapSample() throws ParseException {
 
-		MultiMap<Integer, Person> multiMap = hz.getMultiMap(personsForCompanyIdMap);
+        MultiMap<Integer, Person> multiMap = hz.getMultiMap(personsForCompanyIdMap);
 
-		multiMap.put(1, new Person(1, "Name1", df.parse("06.12.1978")));
-		multiMap.put(1, new Person(2, "Name1", df.parse("06.12.1978")));
-		multiMap.put(1, new Person(2, "Name1", df.parse("06.12.1978")));
+        multiMap.put(1, new Person(1, "Name1", df.parse("06.12.1978")));
+        multiMap.put(1, new Person(2, "Name1", df.parse("06.12.1978")));
+        multiMap.put(1, new Person(2, "Name1", df.parse("06.12.1978")));
 
-		multiMap.put(3, new Person(3, "Name1", df.parse("06.12.1978")));
+        multiMap.put(3, new Person(3, "Name1", df.parse("06.12.1978")));
 
-		Collection<Person> one = multiMap.get(1);
-		System.out.println(one);
-		assertEquals(2, one.size());
+        Collection<Person> one = multiMap.get(1);
+        System.out.println(one);
+        assertEquals(2, one.size());
 
-		System.out.println("---------------------");
+        System.out.println("---------------------");
 
-		multiMap.remove(1, new Person().setId(2));
-		one = multiMap.get(1);
-		System.out.println(multiMap.get(1));
-		assertEquals("Should be removed", 1, one.size());
-	}
+        multiMap.remove(1, new Person().setId(2));
+        one = multiMap.get(1);
+        System.out.println(multiMap.get(1));
+        assertEquals("Should be removed", 1, one.size());
+    }
 
-	@Test
-	public void distributedObjectsListTest() {
+    @Test
+    public void distributedObjectsListTest() {
 
-		hz.getMap("mapName1");
-		hz.getMap("mapName2");
-		hz.getMap("mapName3");
+        hz.getMap("mapName1");
+        hz.getMap("mapName2");
+        hz.getMap("mapName3");
 
-		Collection<DistributedObject> distributedObjects = hz.getDistributedObjects();
+        Collection<DistributedObject> distributedObjects = hz.getDistributedObjects();
 
-		for (DistributedObject distributedObject : distributedObjects) {
-			System.out.println(distributedObject.getName() + "#" + distributedObject.getServiceName());
-		}
+        for (DistributedObject distributedObject : distributedObjects) {
+            System.out.println(distributedObject.getName() + "#" + distributedObject.getServiceName());
+        }
 
-	}
+    }
 
-	@Test
-	public void getMapNameTest() {
-		IMap<Object, Object> map = hz.getMap("h.name");
-		assertEquals("h.name", map.getName());
-	}
+    @Test
+    public void getMapNameTest() {
+        IMap<Object, Object> map = hz.getMap("h.name");
+        assertEquals("h.name", map.getName());
+    }
 
-	@Test
-	public void getQueryCache() {
-		personMap.put(1, new Person().setId(1).setName("Name1"));
-		personMap.put(2, new Person().setId(2).setName("Name2"));
+    @Test
+    public void getQueryCache() {
+        personMap.put(1, new Person().setId(1).setName("Name1"));
+        personMap.put(2, new Person().setId(2).setName("Name2"));
 
-		QueryCache<Integer, Person> c1 = personMap.getQueryCache("C1", TruePredicate.truePredicate(), true);
-		Person p1 = c1.get(1);
-		assertNotNull(p1);
-
-
-		Collection<Person> values = c1.values();
-		System.out.println(values);
+        QueryCache<Integer, Person> c1 = personMap.getQueryCache("C1", TruePredicate.truePredicate(), true);
+        Person p1 = c1.get(1);
+        assertNotNull(p1);
 
 
-	}
+        Collection<Person> values = c1.values();
+        System.out.println(values);
+
+    }
+
+    @Test
+    public void putAndCheckActionTest() {
+
+        Person firstValue = personMap.put(1, new Person(1, "existing.value", null));
+        Person existingValue = personMap.put(1, new Person(2, "new.value", null));
+
+        assertNull(firstValue);
+        assertEquals("existing.value", existingValue.getName());
+
+    }
 
 }
